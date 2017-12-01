988ffb892633c32e9db2dc9ae50424a177c721a6
Carl Closs <ccloss1@cs.binghamton.edu>
2017-11-30 22:00:21 -0500

    main's structure set up

--- CA5.cpp
+++ CA5.cpp
@@ -9,5 +9,24 @@ int main(int argc, char** argv){
 		std::cout << "Format: <requirements> <offerings> <schedule>\n";
 		return 1;
 	}
-
+	std::ifstream reqs(argv[1]), offers(argv[2]), planned(argv[3]);
+	reqs.peek();
+	offers.peek();
+	planned.peek(); //Does not take anything off stream, but we need to make sure these files actually exist.
+	if(reqs.fail() || offers.fail() || planned.fail()){
+		std::cout << "Cannot access one of the files. Please try again!";
+		return 2;
+	}
+	Student student;
+	std::string line;
+	while(!offers.eof()){
+		std::getline(offers, line);
+		//populate courses hash map
+	}
+	while(!reqs.eof()){
+		//set up requirement graph
+	}
+	while(!planned.eof()){
+		//Do actual checking
+	}
 }
