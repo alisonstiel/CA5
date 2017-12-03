6cd2f7bf00b05aebca0ee7bf155d905de3ad959f
Alison Stiel <astiel1@binghamton.edu>
2017-12-03 11:21:23 -0500

    maps for credit counting in Student

--- Student.cpp
+++ Student.cpp
@@ -4,23 +4,55 @@
 #include <unordered_map>
 #include "Student.h"
 
+Student::Student(){
+	requiredCredits["total"] = 0;
+	scheduleCredits["total"] = 0;	
+}
+
 void Student::addRequirement(std::string courseName){
     requirements[courseName] = false;
 }
+
 void Student::addToSchedule(Course course){
     schedule[course.getName()] = course;
 }
+
 void Student::addCourse(Course course){
     courses[course.getName()] = course;
 }
+
+void Student::addRequiredCredits(std::string reqType, int credits){
+	if(credits > 0){
+		requiredCredits["total"] += credits;
+		requiredCredits[reqType] += credits; //creates a new key if reqType doesn't yet exist, or increments it's value if it does exist 	
+	}
+}
+
+void Student::addScheduleCredits(std::string reqType, int credits){
+	if(credits > 0){
+		scheduleCredits["total"] += credits;
+		scheduleCredits[reqType] += credits;	
+	}
+}
+
 std::unordered_map<std::string, bool> Student::getRequirements(){
     return requirements;
 }
+
 std::unordered_map<std::string, Course> Student::getSchedule(){
     return schedule;
 }
+
 std::unordered_map<std::string, Course> Student::getCourses(){
     return courses;
+}
+
+std::unordered_map<std::string, int> Student::getRequiredCredits(){
+    return requiredCredits;
+}
+
+std::unordered_map<std::string, int> Student::getScheduleCredits(){
+    return scheduleCredits;
 }
 
 std::string Student::findLackingPrereq(const std::string& courseName){
--- Student.h
+++ Student.h
@@ -5,15 +5,22 @@
 #include "Course.h"
 class Student{
 	public:
+		Student();
 		void addRequirement(std::string courseName); 
 		void addToSchedule(Course course); 
 		void addCourse(Course course);
+		void addRequiredCredits(std::string reqType, int credits);
+		void addScheduleCredits(std::string reqType, int credits);
 		std::unordered_map<std::string, bool> getRequirements();
 		std::unordered_map<std::string, Course> getSchedule();
 		std::unordered_map<std::string, Course> getCourses();
+		std::unordered_map<std::string, int> getRequiredCredits();	
+		std::unordered_map<std::string, int> getScheduleCredits();
 	private:
 		std::unordered_map<std::string, bool> requirements; //student's course requirements in order to graduate
 		std::unordered_map<std::string, Course> schedule; //student's complete planned schedule
-		std::unordered_map<std::string, Course> courses; //all possible courses offered	
+		std::unordered_map<std::string, Course> courses; //all possible courses offered
+		std::unordered_map<std::string, int> requiredCredits; //map of student's required credits types: total, H, C, F...
+		std::unordered_map<std::string, int> scheduleCredits; //map of student's schedule's credits types: total, H, C, F...
 		std::string findLackingPrereq(const std::string& courseName);	
 };
